0d6c0cdc86b683b31018b054d0106e7a19a74119
Ubuntu <ubuntu@ip-172-31-22-137.us-west-1.compute.internal>
2025-07-03 09:04:13 +0000

    feat: Setup Tailwind CSS build process

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/**/*.{html,js}", // 扫描 dist 目录下的所有 html 和 js 文件
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
